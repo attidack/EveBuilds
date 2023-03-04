2b4727c73771405f55d6ed3832b924709db042b9
Jon Bird <attid@Jons-Laptop>
2023-03-04 13:32:26 -0700

    inital commit

---
+++ client/src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { ApolloProvider } from "react-apollo";
+
+import client from "./apollo";
+import Builds from "./components/Builds";
+
+const App = () => {
+  return (
+    <ApolloProvider client={client}>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Builds} />
+        </Switch>
+      </Router>
+    </ApolloProvider>
+  );
+};
+
+export default App;
---
+++ client/src/components/Builds.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Builds = () => {
+  return <div>Builds Page</div>;
+};
+
+export default Builds;
