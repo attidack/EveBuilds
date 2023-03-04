c2b829c8fcd1303466f61eae320209e83b993f45
Jon Bird <attid@Jons-Laptop>
2023-03-04 14:23:20 -0700

    new errors

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,17 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import { ApolloProvider } from "react-apollo";
 import client from "./apollo";
 import Builds from "./components/Builds";
 
-function App() {
+const App = () => {
   return (
     <ApolloProvider client={client}>
       <Router>
-        <Switch>
-          <Route exact path="/" component={Builds} />
-        </Switch>
+        <Route exact path="/" component={Builds} />
       </Router>
     </ApolloProvider>
   );
-}
+};
 
 export default App;
